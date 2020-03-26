6c0691a1ae7b1ef27670af13d9a0af940afadc53
Emanuele Parrinello <parry84@gmail.com>
2020-03-26 22:17:51 +0100

    add solution to rna-transcription

---
+++ rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,24 @@
+defmodule RnaTranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    dna |> Enum.map(&nucleotide_to_rna/1)
+  end
+
+  @spec nucleotide_to_rna(char) :: char
+  defp nucleotide_to_rna(dna) do
+    case dna do
+      ?G -> ?C
+      ?C -> ?G
+      ?T -> ?A
+      ?A -> ?U
+    end
+  end
+end
