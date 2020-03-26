d7c68983463c43e7332a12b8086aa436618d4940
Emanuele Parrinello <parry84@gmail.com>
2020-03-26 23:29:49 +0100

    add Keyword-based solution to resistor-color problem

--- resistor-color/lib/resistor_color.ex
+++ resistor-color/lib/resistor_color.ex
@@ -1,11 +1,26 @@
 defmodule ResistorColor do
   @moduledoc false
-
-  @spec colors() :: list(String.t())
-  def colors do
-  end
+  @color_code [
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  ]
 
   @spec code(String.t()) :: integer()
   def code(color) do
+    Keyword.get(@color_code, String.to_existing_atom(color))
+  end
+
+  @spec colors() :: list(String.t())
+  def colors do
+    Keyword.keys(@color_code)
+    |> Enum.map(&to_string/1)
   end
 end
